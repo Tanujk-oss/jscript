1d7bc1c93ae52f994ed3522fe1ffcc461381ff9d
Tanujk-oss <tanujkumar6754@gmail.com>
2026-02-25 17:38:12 +0000

    datatype

---
+++ .devcontainer/basics/02_datatypes.js
@@ -0,0 +1,10 @@
+"use strict";    // to treat code as newer version of js
+
+console.log( 3 +
+    3)              // code readability should be high
+
+  // symbols are used to create unique identifiers
+
+console.log(typeof null)    // object
+console.log(typeof undefined)   // undefined 
+
