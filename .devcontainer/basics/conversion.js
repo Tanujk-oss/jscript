82fd68f42854f5957a47acd44d60fe0f86103e75
Tanujk-oss <tanujkumar6754@gmail.com>
2026-02-26 16:10:13 +0000

    conversion of datatypes

---
+++ .devcontainer/basics/conversion.js
@@ -0,0 +1,28 @@
+let score = null
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+// let num1 = Number(score)
+// console.log(typeof(num1))
+// console.log(num1)
+
+//  "33" => 33
+// "33abc" = NaN
+// true => 1
+// null => 0
+
+let isLoggedIn = "tanuj"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 0 => false 1 => true
+// "" => false "tanuj" => true
+
+
+let someNumber = 3356
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
