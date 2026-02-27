838384c2a9b50657f088ed0f750003465b3b7415
Tanujk-oss <tanujkumar6754@gmail.com>
2026-02-27 17:10:34 +0000

    datatypes_Summary

--- .devcontainer/basics/datatypeSummary.js
+++ .devcontainer/basics/datatypeSummary.js
@@ -2,21 +2,21 @@
 // undefined,symbol
 
 const ac = 5678 // dynamically typed
-const id = symbol('1234')
-const anotherID = symbol('1234')
+const id = Symbol('1234')
+const anotherID = Symbol('1234')
 
 let name = undefined  
-let name     // undefined
+let name2     // undefined
 
 const bigNumber = 12345459869849n  // BigInt
 
 // non-primitive  (call by refrence)
 // arrays, functions and objects
 
- const heroes = ['superman', 'batman', 'spiderman']
+ const heroes =["superman", "batman", "spiderman"]
  
  const person = {
-    name: 'John',         //key value pair object
+    name: "John",         //key value pair object
     age: 30
  }
 
@@ -24,7 +24,25 @@ const bigNumber = 12345459869849n  // BigInt
     console.log('Hello World')
  }
 
-
  console.log(typeof myFunction)  // function  
 console.log(typeof heroes)// object
 console.log(typeof anotherID)// symbol
+
+// ***********************************************************************************
+// stack (primitive), Heap (non-primitive)
+
+let ytname = "HiteshChaudhary"
+let anotherName = ytname  // copy of the value
+anotherName = "ChaiAurCode"
+console.log(ytname)  // HiteshChaudhary
+console.log(anotherName)  //ChaiAurCode
+
+let user = {
+      name: "Hitesh",
+      upi: "hitesh@ybl"
+}
+
+user2 = user
+user2.name = "HiteshChaudhary"
+console.log(user.name)  // HiteshChaudhary
+console.log(user2.name) // HiteshChaudhary
